27dbaae808cc08a74f226ddebec7c3ca0a2abe1f
KANAK MALVIYA <60460750+Kanakmalviya04@users.noreply.github.com>
2022-10-01 20:08:46 +0530

    Project Added ✔

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+      <div className="card footer">
+        <h2>GitFind | Developed by Kanak &amp; Team | &copy; 2022</h2>
+        <a href="https://github.com/Kanakmalviya04/Gitfind" target="_blank" rel="noreferrer">Github Source Code</a>
+        <p> Made with ❤️ &amp; 💻</p>
+      </div>
+    )
+  }
+  
+export default Footer;
